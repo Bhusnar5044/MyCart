b5e111ffd00747dea97286550dbc98f564f73db0
Bhusnar5044 <bhusnarpradip@gmail.com>
2020-11-05 18:30:53 +0530

    hosted

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { PersistGate } from "redux-persist/integration/react";
 import ApplicationStore,{persistor}from './Redux/store'
+import axe from "react-axe";
+
+if (process.env.NODE_ENV !== "production") {
+  axe(React, ReactDOM, 1000);
+}
 
 ReactDOM.render(
   <React.StrictMode>
+    <HashRouter basename={process.env.PUBLIC_URL}>
     <Provider store={ApplicationStore}>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
