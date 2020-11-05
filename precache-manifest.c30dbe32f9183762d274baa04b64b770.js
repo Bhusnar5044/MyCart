b8dc139d9be8cb1c39ddecd41d71a17e4ef03632
Bhusnar5044 <bhusnarpradip@gmail.com>
2020-11-05 18:26:31 +0530

    Updates

---
+++ precache-manifest.c30dbe32f9183762d274baa04b64b770.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738461356bcba643dfaf58e999795148",
+    "url": "/MyCart/index.html"
+  },
+  {
+    "revision": "024998bd349cbcf4120f",
+    "url": "/MyCart/static/css/main.dc574072.chunk.css"
+  },
+  {
+    "revision": "849fbfb0b70f932de87e",
+    "url": "/MyCart/static/js/2.7659e39e.chunk.js"
+  },
+  {
+    "revision": "3592243fa4151d49661d0f250fc9b399",
+    "url": "/MyCart/static/js/2.7659e39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024998bd349cbcf4120f",
+    "url": "/MyCart/static/js/main.a3ab473c.chunk.js"
+  },
+  {
+    "revision": "0f9ab3a7b72fb0fce691",
+    "url": "/MyCart/static/js/runtime-main.6764e873.js"
+  },
+  {
+    "revision": "d0d4b209109a6da9c0bf002ac18aa635",
+    "url": "/MyCart/static/media/404.d0d4b209.jpg"
+  },
+  {
+    "revision": "3dcd56f4964eb5bb785e47c981850635",
+    "url": "/MyCart/static/media/empty.3dcd56f4.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/MyCart/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "27dd9c0ae9916d252004b7a1b10551c5",
+    "url": "/MyCart/static/media/user.27dd9c0a.png"
+  }
+]);
